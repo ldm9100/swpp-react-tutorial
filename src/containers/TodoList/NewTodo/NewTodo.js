2bb8873296f2083a00d5ba534b716d24d6df4d94
ldm9100 <ldm9100@naver.com>
2022-10-16 17:40:59 +0900

    final commit

--- src/containers/TodoList/NewTodo/NewTodo.js
+++ src/containers/TodoList/NewTodo/NewTodo.js
@@ -15,7 +15,7 @@ class NewTodo extends Component {
         return (
             <div className="NewTodo">
                 <h1>Add a Todo</h1>
-                <label>Titlee</label>
+                <label>Title</label>
                 <input type="text" value={this.state.title}
                 onChange={(event) => this.setState({ title: event.target.value})} />
                 <label>Content</label>
